012c3eb89ff0f099e4d16b2d01032cd39c7a6d37
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-01-18 21:40:16 -0500

    Delete top_cocktail.rb

--- top_cocktail.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-require "bundler/setup"
-
-require_relative './top_cocktail/version'
-require_relative './top_cocktail/cocktail'
-require_relative './top_cocktail/cli'
-require_relative './top_cocktail/scraper'
